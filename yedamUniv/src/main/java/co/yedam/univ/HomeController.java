61e3e8a5ef16bdbf082f80514077fc669a384f58
twingtwing <qazxc753@gmail.com>
2022-01-19 17:59:24 +0900

    commit-s

--- yedamUniv/src/main/java/co/yedam/univ/HomeController.java
+++ yedamUniv/src/main/java/co/yedam/univ/HomeController.java
@@ -1,6 +1,7 @@
 package co.yedam.univ;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,18 @@ public class HomeController {
 	public String home() {
 		return "home/main";
 	}
-	
+
+	@GetMapping("/stu/stu.do")
+	public String stuPage() {
+		return "stu/home/stuMain";
+	}
+	@GetMapping("/pro/pro.do")
+	public String proPage() {
+		return "pro/home/proMain";
+	}
+
+	@GetMapping("/admin/admin.do")
+	public String admin() {
+		return "admin/home/adminMain";
+	}
 }
